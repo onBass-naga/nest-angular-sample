1787173701b1e5264bb16e54d64951d9c906a65a
naga <naga@area-b.com>
2018-09-11 19:48:11 +0900

    Add TasksModule to AppModule. And delete files unused.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [LoggerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    LoggerModule,
+    TasksModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
